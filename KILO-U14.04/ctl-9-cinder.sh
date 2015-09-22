a4452af0f2cedba5aa4e45740784acdcd181b6be
Tô Tướng Công <tcvn1985@gmail.com>
2015-09-22 13:30:30 +0700

    update config cinder

--- KILO-U14.04/ctl-9-cinder.sh
+++ KILO-U14.04/ctl-9-cinder.sh
@@ -1,18 +1,40 @@
 #!/bin/bash -ex
 source config.cfg
 
-apt-get install lvm2 -y
+## Create a cinder user, endpoint
+openstack user create --password $ADMIN_PASS cinder
+openstack role add --project service --user cinder admin
+openstack service create --name cinder --description "OpenStack Block Storage" volume
+openstack service create --name cinderv2  --description "OpenStack Block Storage" volumev2
+
+openstack endpoint create \
+--publicurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--internalurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--adminurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--region RegionOne \
+volume
+
+
+openstack endpoint create \
+--publicurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--internalurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--adminurl http://$CON_MGNT_IP:8776/v2/%\(tenant_id\)s \
+--region RegionOne \
+volumev2
+
+  
+# apt-get install lvm2 -y
 
 echo "########## Create Physical Volume and Volume Group (in sdb disk ) ##########"
-fdisk -l
-pvcreate /dev/vdb
-vgcreate cinder-volumes /dev/vdb
+# fdisk -l
+# pvcreate /dev/vdb
+# vgcreate cinder-volumes /dev/vdb
 
 #
 echo "########## Install CINDER ##########"
 sleep 3
-apt-get install -y cinder-api cinder-scheduler cinder-volume iscsitarget open-iscsi iscsitarget-dkms python-cinderclient
-
+# apt-get install -y cinder-api cinder-scheduler cinder-volume iscsitarget open-iscsi iscsitarget-dkms python-cinderclient
+apt-get install -y cinder-api cinder-scheduler python-cinderclient
 
 echo "########## Configuring for cinder.conf ##########"
 
@@ -60,19 +82,21 @@ lock_path = /var/lock/cinder
 
 EOF
 
-sed  -r -e 's#(filter = )(\[ "a/\.\*/" \])#\1[ "a\/sda1\/", "a\/sdb\/", "r/\.\*\/"]#g' /etc/lvm/lvm.conf
+# sed  -r -e 's#(filter = )(\[ "a/\.\*/" \])#\1[ "a\/sda1\/", "a\/sdb\/", "r/\.\*\/"]#g' /etc/lvm/lvm.conf
 
 # Grant permission for cinder
 chown cinder:cinder $filecinder
 
 echo "########## Syncing Cinder DB ##########"
 sleep 3
-cinder-manage db sync
-
+su -s /bin/sh -c "cinder-manage db sync" cinder
+ 
 echo "########## Restarting CINDER service ##########"
 sleep 3
 service cinder-api restart
 service cinder-scheduler restart
-service cinder-volume restart
+# service cinder-volume restart
+
+rm -f /var/lib/cinder/cinder.sqlite
 
 echo "########## Finish setting up CINDER !!! ##########"
